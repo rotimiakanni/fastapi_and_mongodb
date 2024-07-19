b075e20c32bd2138b82a22035a430417db8a5dd3
Kafayat Lawal <kafayatlawal112@gmail.com>
2024-07-19 01:48:35 +0100

    Added authentication to book endpoints

--- schema.py
+++ schema.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
+from bson.objectid import ObjectId
 
 class BookBase(BaseModel):
     title: str
@@ -9,17 +10,24 @@ class BookBase(BaseModel):
 class Book(BookBase):
     id: str
         
-class BookCreate(BookBase):
+class BookCreatePayload(BookBase):
     pass
 
+class BookCreate(BookBase):
+    user_id: str
+
 class BookUpdate(BookBase):
+    title: Optional[str] = None
+    author: Optional[str] = None
     description: Optional[str] = None
 
 # User
 class UserBase(BaseModel):
+    id: str
     username: str
 
-class UserCreate(UserBase):
+class UserCreate(BaseModel):
+    username: str
     full_name: str
     password: str
 
