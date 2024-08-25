9de490939427a1e2fe5d06abb2e85e25a56adf7c
Nkwopara Paachal <paschal0623chizi@gmail.com>
2024-08-25 08:45:12 +0100

    Added authentication to the Book endpoints

--- auth.py
+++ auth.py
@@ -1,5 +1,4 @@
 import os
-
 from datetime import datetime, timedelta
 from typing import Optional
 from jose import JWTError, jwt
@@ -9,27 +8,42 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 
+# Import the CRUD service for user-related operations
 from crud import user_crud_service
 
+# Load environment variables from a .env file
 load_dotenv()
 
-SECRET_KEY = os.environ.get('SECRET_KEY')  # Change this in production
+# Get sensitive information from environment variables
+SECRET_KEY = os.environ.get('ea0884bb237ae9628f777bf19ad196d5af8851ee9500ba3b9723df9337d5cec2')  # Change this in production
 ALGORITHM = os.environ.get('ALGORITHM')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', 30))
 
+# Set up password hashing context
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# OAuth2 scheme for getting the token from the request header
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Verify a plain password against a hashed password.
+    """
     return pwd_context.verify(plain_password, hashed_password)
 
 def authenticate_user(username: str, password: str):
+    """
+    Authenticate a user given their username and password.
+    """
     user = user_crud_service.get_user_by_username_with_hash(username)
     if not user or not verify_password(password, user.get('hashed_password')):
         return False
     return user
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    """
+    Create a JWT token with the provided data and expiration time.
+    """
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -40,6 +54,9 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
+    """
+    Get the current user from the JWT token provided in the request header.
+    """
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
